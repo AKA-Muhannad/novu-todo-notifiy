86eb4c3caca6a74bf39875255542bf2bef3b18f9
AKA-Muhannad <muhannadalseraye@gmail.com>
2024-04-27 19:41:11 +0300

    add api todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 type Props = {
     email: string,
@@ -12,20 +13,52 @@ interface TodoItem {
     todoHeading: string
 }
 
+const url = process.env.REACT_APP_TODO_URL!
+
+console.log(url)
+
 function App() {
     const [email, setEmail] = useState('')
     const [todoHeading, setTodoHeading] = useState('')
     const [todo, setTodo] = useState<TodoItem[]>([])
 
-    const addTodo = () => {
-        if (!email || !todoHeading) {
-            setTodo([...todo, {email: email, todoHeading: todoHeading}])
+    const addTodo = async (e: any) => {
+        e.preventDefault();
+        try {
+             await axios.post(`${url}/add-Todo`, {
+                todoHeading: todoHeading,
+                email: email
+            })
+            if (email && todoHeading) setTodo([...todo, {email: email, todoHeading: todoHeading}])
+
+        } catch (error) {
+            console.log(error)
         }
+
     }
 
     useEffect(() => {
         console.log(todoHeading)
     }, [todoHeading]);
+
+    useEffect(() => {
+        console.log(todo)
+    }, [todo]);
+
+    useEffect(() => {
+        const getTodos = async () => {
+            try {
+                const res = await axios.get(`${url}/all-Todo`)
+                console.log(res)
+            } catch (e) {
+                console.log(e)
+            }
+        }
+        getTodos().then(result => {
+            console.log(result)
+        }).catch((error) => console.log(error))
+    }, []);
+
     return (
         <div className='parent'>
             {/*<div className='headers'>*/}
@@ -37,10 +70,12 @@ function App() {
             {/*</div>*/}
             <div className="todo_wrapper">
                 <div>
-                    <form onSubmit={addTodo}>
+                    <form onSubmit={(e: React.FormEvent<HTMLFormElement>) => addTodo(e)}>
                         <input className='input' type="text" placeholder={'Todo'}
-                               onChange={(e) => setTodoHeading(e.target.value)}/>
-                        <input type="text" placeholder="Email" onChange={(e) => setEmail(e.target.value)}
+                               required
+                               onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTodoHeading(e.target.value)}/>
+                        <input type="text" placeholder="Email" required
+                               onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
                         />
 
                         <button type="submit" className='addButton'>+</button>
@@ -49,10 +84,10 @@ function App() {
 
                 <div className='todo_container'>
                     {/* All the todos  */}
-                    {todo.map((val, index) => {
-                        return(
-                            <div>
-                                {val.email} {val.todoHeading}
+                    {todo.map((val: TodoItem, index: number) => {
+                        return (
+                            <div key={index}>
+                                {index + 1} - {val.todoHeading} {val.email}
                             </div>
                         )
                     })}
